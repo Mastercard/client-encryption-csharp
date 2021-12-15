5b29f5e327664842255b5eef6883448a07b00521
Jean-Alexis Aufauvre <jean-alexis.aufauvre@mastercard.com>
2021-12-15 14:15:22 +0000

    Added a .NET 5 test project, moved all tests to the .NET Core 2.1 project to properly test the AES GCM support

---
+++ Mastercard.Developer.ClientEncryption.Tests/NetCore2/JWE/GCMJweObjectTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Mastercard.Developer.ClientEncryption.Core.Encryption;
+using Mastercard.Developer.ClientEncryption.Core.Encryption.JWE;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mastercard.Developer.ClientEncryption.Tests.NetCore.Test;
+
+namespace Mastercard.Developer.ClientEncryption.Tests.NetCore.Encryption.JWE
+{
+    [TestClass]
+    public class GCMJweObjectTest
+    {
+        [TestMethod]
+#if !NETCOREAPP3_1 && !NET5_0_OR_GREATER
+        [ExpectedException(typeof(EncryptionException), "AES/GCM/NoPadding is unsupported on .NET Standard < 2.1")]
+#endif
+        public void TestDecrypt_ShouldReturnDecryptedPayload_WhenPayloadIsGcmEncrypted()
+        {
+            // GIVEN
+            JweObject jweObject = TestUtils.GetTestGcmJweObject();
+
+            // WHEN
+            string decryptedPayload = jweObject.Decrypt(TestUtils.GetTestJweConfigBuilder().Build());
+
+            // THEN
+            Assert.AreEqual("{\"foo\":\"bar\"}", decryptedPayload);
+        }
+    }
+}
